05b36171c18a84c4717aebdf333c8b26a8e968e0
tolga.tuna <tolga.tuna@hotmail.com>
2017-11-27 01:36:07 -0800

    Chapter #4 - Higher Order Components (HOC) require_authentication is added.

---
+++ src/components/require_authentication.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+export default function (ComposedComponent) {
+    class Authentication extends Component {
+        static contextTypes = {
+            router: React.PropTypes.object
+        };
+
+        componentWillMount() {
+            if(!this.props.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+        componentWillUpdate(nextProps) {
+            if(!nextProps.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+        render() {
+            return this.props.authenticated ?
+                <ComposedComponent {...this.props}/> :
+                null;
+        }
+    }
+
+    function mapStateToProps(state) {
+        return {
+            authenticated: state.authenticated
+        };
+    }
+
+    return connect(mapStateToProps)(Authentication);
+}
+
